f45fe54315bf7ff3b1c8e5d872b629c76b661f7a
rlaksl <eehiuh@gmail.com>
2025-12-15 23:54:04 +0900

    Setting: 프로젝트 초기화 및 DB 연결

---
+++ src/main/java/com/ticket/ConcertTicketingBeApplication.java
@@ -0,0 +1,13 @@
+package com.ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertTicketingBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertTicketingBeApplication.class, args);
+	}
+
+}
