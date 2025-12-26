2e8477575abd42e5c468c665ded60e95bd874a6b
rlaksl <eehiuh@gmail.com>
2025-12-26 14:33:44 +0900

    fix: 테스트 환경 Redis 설정 추가

---
+++ src/test/java/com/ticket/config/TestConfig.java
@@ -0,0 +1,21 @@
+package com.ticket.config;
+
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@TestConfiguration
+public class TestConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return Mockito.mock(RedisConnectionFactory.class);
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+        return Mockito.mock(RedisTemplate.class);
+    }
+}
